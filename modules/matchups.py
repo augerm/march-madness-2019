e099b036dcabdbed37f1354162088fbb6dad0cc1
Aakaash <aakaash.meduri@gmail.com>
2019-03-10 20:52:08 -0500

    aakaash may or may not have fucked some shit up

--- modules/matchups.py
+++ modules/matchups.py
@@ -10,12 +10,12 @@ class Matchups:
     def __init__(self):
         self.teams_dict = {}
         
-    def get_matchups(self):
-        match_year = 2018
+    def get_matchups(self, match_year):
         data = pd.read_csv(input_file)
         data = data[data['Season'] == match_year]
         teamIDs = list(data['TeamID'].sort_values())
         matchups = []
+
         for i in range(len(teamIDs)):
             for j in range(i+1, len(teamIDs)):
 
@@ -33,9 +33,8 @@ class Matchups:
                     teamB = Team(match_year, teamIDs[j])
                     self.teams_dict[teamB_key] = teamB
 
-                match = Match(match_year=match_year, teamA=teamA, teamB=teamB)
-                teamA.add_match(match)
-                teamB.add_match(match)
+                match = Match(match_year, teamA, teamB, 0)
+
                 matchups.append(match)
         return matchups
 
@@ -45,11 +44,36 @@ class Matchups:
         for i in range(len(data)):
             line = data.loc[i]
             if line['WTeamID'] < line['LTeamID']:
-                completed_matchups.append(CompletedMatch(year = line['Season'], teamA = line['WTeamID'], teamB = line['LTeamID'], 
+                completed_matchups.append(CompletedMatch(year = line['Season'], teamA = line['WTeamID'], teamB = line['LTeamID'],
                     scoreA = line['WScore'], scoreB = line['LScore'], result = True, day_num = line['DayNum']))
+                year = line['Season']
+                teamA_id = line['WTeamID']
+                teamA_score = line['WScore']
+                teamB_id = line['LTeamID']
+                teamB_score = line['LScore']
+                day_num = line['DayNum']
+                teamA_key = Team.get_key(year, teamA_id)
+                teamB_key = Team.get_key(year, teamB_id)
+                match = Match(year, teamA_id, teamB_id, day_num, teamA_score, teamB_score)
+                teamA.add_completed_match(match)
+                teamB.add_completed_match(match)
+
+
             else:
                 completed_matchups.append(CompletedMatch(year = line['Season'], teamB = line['WTeamID'], teamA = line['LTeamID'], 
                     scoreB = line['WScore'], scoreA = line['LScore'], result = False, day_num = line['DayNum']))
+                year = line['Season']
+                teamA_id = line['LTeamID']
+                teamA_score = line['LScore']
+                teamB_id = line['WTeamID']
+                teamB_score = line['WScore']
+                day_num = line['DayNum']
+                teamA_key = Team.get_key(year, teamA_id)
+                teamB_key = Team.get_key(year, teamB_id)
+                match = Match(year, teamA_id, teamB_id, day_num, teamA_score, teamB_score)
+                teamA.add_completed_match(match)
+                teamB.add_completed_match(match)
+
         return completed_matchups
 
     def build_teams_dict(self):
--- modules/output_generator.py
+++ modules/output_generator.py
@@ -2,8 +2,8 @@ class OutputGenerator:
     def __init__(self):
         self.output_file = "output/output.txt"
 
-    def write_result(self, matches):
+    def write_results(self, matches):
         with open(self.output_file, 'w') as f:
             f.write("id,pred\n")
             for match in matches:
-                f.write("{}_{}_{},{}".format(match.year, match.teamA, match.teamB, match.result))
+                f.write("{}_{}_{},{}\n".format(match.year, match.teamA.team_id, match.teamB.team_id, match.predicted_result))
