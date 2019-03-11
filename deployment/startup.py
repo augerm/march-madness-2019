4dc9c7a9b62deb23317944cfa05c4985b38275db
Michael Auger <michaelauger@Michaels-MBP.attlocal.net>
2019-03-11 18:45:31 -0500

    Add logging library

--- deployment/startup.py
+++ deployment/startup.py
@@ -1,12 +1,23 @@
 import os
+import logging
 
-print("@@@ Running startup script...")
-print("@@@ Fetching origin...")
+# Imports the Google Cloud client library
+import google.cloud.logging
+
+# Instantiates a client
+client = google.cloud.logging.Client()
+
+# Connects the logger to the root logging handler; by default this captures
+# all logs at INFO level and higher
+client.setup_logging()
+
+logging.info("@@@ Running startup script...")
+logging.info("@@@ Fetching origin...")
 os.system('git pull origin master...')
 
-print("@@@ Running main.py...")
+logging.info("@@@ Running main.py...")
 os.system('python3 ../main.py')
 
 # Turn off machine when finished in order to limit costs.
-print("@@@ Shutting down machine...")
+logging.info("@@@ Shutting down machine...")
 os.system('sudo poweroff')
