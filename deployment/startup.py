50e3aa098696f39e31e4adbcb79b51d5a3ba14ec
augerm <michaelauger23@gmail.com>
2019-03-12 23:21:40 -0500

    Yo

--- deployment/startup.py
+++ deployment/startup.py
@@ -5,13 +5,16 @@ print("@@@ Running startup script...")
 print("@@@ Navigating to project folder")
 os.system('cd /home/michaelauger23/march-madness-2019')
 
+print("@@@ Printing working directory")
+os.system('pwd')
+
 print("@@@ Downloading data files")
 # os.system('gsutil cp -r gs://march-madness-2019 /home/michaelauger23/march-madness-2019')
 
 print("@@@ Fetching origin...")
 os.system('git pull origin master')
 
-print("@@@ Running main.py...")
+print("@@@ Running train.py...")
 os.system('python3 ./train.py')
 
 # Turn off machine when finished in order to limit costs.
