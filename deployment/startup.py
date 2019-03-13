1c538a183661d3e2b5a9ee3cfde8861c70c007b3
augerm <michaelauger23@gmail.com>
2019-03-12 22:00:12 -0500

    Update startup script to pull latest

--- deployment/startup.py
+++ deployment/startup.py
@@ -1,11 +1,15 @@
 import os
 
 print("@@@ Running startup script...")
+
+print("@@@ Navigating to project folder")
+os.system('cd /home/michaelauger23/march-madness-2019')
+
 print("@@@ Fetching origin...")
-print('git pull origin master...')
+os.system('git pull origin master')
 
 print("@@@ Running main.py...")
-os.system('python3 ../modules/train.py')
+os.system('python3 ./modules/train.py')
 
 # Turn off machine when finished in order to limit costs.
 print("@@@ Shutting down machine...")
