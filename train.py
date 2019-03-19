2b73bbe6c29fff959d5dbc27e5a4956d6b548217
augerm <michaelauger23@gmail.com>
2019-03-19 18:55:15 +0000

    Divide training, testing and validation

--- train.py
+++ train.py
@@ -9,21 +9,26 @@ from modules.loss_function import get_loss_single
 # maybe also load test data, and predict data!
 def get_train_data():
     matchups = Matchups()
-    completed_matchups = matchups.get_completed_matchups(until_year=2015)
-    train_x = list(map(lambda completed_matchup: completed_matchup.get_features(), completed_matchups))
-    train_y = list(map(lambda completed_matchup: completed_matchup.result, completed_matchups))
-    train_x = train_x[:10000]
-    train_y = train_y[:10000]
+    completed_matchups = matchups.get_completed_matchups(until_year=2019)
+    split = 10000
+    data_x = list(map(lambda completed_matchup: completed_matchup.get_features(), completed_matchups))
+    data_y = list(map(lambda completed_matchup: completed_matchup.result, completed_matchups))
+    train_x = data_x[:split]
+    train_y = data_y[:split]
+    test_x  = data_x[split:split+split]
+    test_y  = data_y[split:split+split]
 
     # normalize data
     train_x = np.array(train_x)
     train_y = np.array(train_y)
+    test_x = np.array(test_x)
+    test_y = np.array(test_y)
     print("finished loading train data")
-    return np.array(train_x), np.array(train_y)
+    return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
 
 
 def train_model():
-    train_x, train_y = get_train_data()
+    train_x, train_y, test_x, test_y = get_train_data()
     print(len(train_x), len(train_y))
     # Create neural network architecture
     model = k.Sequential()
@@ -35,12 +40,12 @@ def train_model():
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=[metrics.binary_accuracy])
     model.fit(np.array(train_x), np.array(train_y), validation_split=0.25, epochs=40, batch_size=100)
 
-    test_loss, test_acc = model.evaluate(np.array(train_x), np.array(train_y))
+    test_loss, test_acc = model.evaluate(test_x, test_y)
 
     print('Test accuracy:', test_acc)
+    print('Test loss:', test_loss)
 
     date_str = str(datetime.datetime.now().strftime("%d-%B-%Y-%I-%M%p"))
     model.save('keras_models/{}.h5'.format(date_str))
 
-# train_model()
-get_train_data()
+train_model()
